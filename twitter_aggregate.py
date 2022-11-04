b144b8cc51372f73a4496e9618044c4507e152b4
E-dmp <ridemdem1510@gmail.com>
2022-11-04 22:02:26 +0900

    feat:Twitter認証機能の作成 #1

--- twitter_aggregate.py
+++ twitter_aggregate.py
@@ -2,6 +2,12 @@ import tweepy
 from authentication_module import get_authentication_info
 
 #Twitterの認証
+api_key = get_authentication_info("api_key")
+api_secret = get_authentication_info("api_secret")
+access_key = get_authentication_info("access_key")
+access_secret = get_authentication_info("access_secret")
 
-
+auth = tweepy.OAuthHandler(api_key, api_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth)
 
