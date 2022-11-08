052f54f24a0497fbc34a499d63cf7b9508378d22
E-dmp <ridemdem1510@gmail.com>
2022-11-08 22:02:18 +0900

    get-hashtag-tweet #5

--- twitter_aggregate.py
+++ twitter_aggregate.py
@@ -5,14 +5,42 @@ from authentication_module import get_authentication_info
 api_key = get_authentication_info("api_key")
 api_secret = get_authentication_info("api_secret")
 access_token = get_authentication_info("access_token")
-access_token_secret = get_authentication_info("access_secret")
+access_token_secret = get_authentication_info("access_token_secret")
 
 auth = tweepy.OAuthHandler(api_key, api_secret)
-auth.set_access_token(access_key, access_secret)
+auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-# テストでツイート取得
-tweet_objects = api.search_tweets(q="漫画",result_type="recent",count=5)
+# ツイート取得
+SEARCH_KEYWORD = "#漫画がよめるハッシュタグ -filter:retweets"
+tweet_objects = api.search_tweets(q=SEARCH_KEYWORD,result_type="recent",count=100)
 
-for tweet_object in tweet_objects:
-    print(tweet_object.text)
+# ツイートソート
+LIKES_COUNTS = 500
+
+tweetids_with_more_than_thousand_likes = []
+accounts_with_more_than_thousand_likes = []
+
+
+def sortTweet(tweet_objects):
+    """
+    リツイート,メンションを除く
+    1000以上のいいねがついたuser,idを返却
+    """
+    for tweet in tweet_objects:
+
+        if tweet.favorite_count <= LIKES_COUNTS:
+            continue
+
+        tweetids_with_more_than_thousand_likes.append(tweet.id)
+        accounts_with_more_than_thousand_likes.append(tweet.user.screen_name)
+
+    return [accounts_with_more_than_thousand_likes,tweetids_with_more_than_thousand_likes]
+
+accounts,tweetids= sortTweet(tweet_objects)
+
+print(accounts)
+print(tweetids)
+
+
+
